2c3d0cf37f6b5a2489b9b1305a8d72793d1b96f0
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-02-24 12:40:18 -0500

    added thought model with index.js

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create a course model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Reaction",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;

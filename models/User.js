8191959628a3458626882172f053c487a4d4a4f8
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-02-24 12:22:28 -0500

    setup user model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const { Schema, Types } = require("mongoose");
+
+const userSchema = new Schema({
+  userID: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: () => Promise.resolve(false),
+      message: "Email validation failed",
+    },
+  },
+  thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+  friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+});
+
+module.exports = userSchema;

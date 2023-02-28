1f8f9a79ea05c82583af647a7375b849e5e50c51
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-02-28 10:58:51 -0500

    fixed thought schema to include reactionSchema

--- models/Thought.js
+++ models/Thought.js
@@ -43,12 +43,7 @@ const thoughtSchema = new Schema(
       type: String,
       required: true,
     },
-    reactions: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Reaction",
-      },
-    ],
+    reactions: [reactionSchema],
   },
   {
     toJSON: {
